72037e8df03f3be4a62117f8ea0dc350d8f7071a
kimmvb <kimveb1@gmail.com>
2023-11-01 10:01:10 -0500

    últimos ajustes del proyecto (hasta el hito 3)

--- test/extract_files.spec.js
+++ test/extract_files.spec.js
@@ -4,21 +4,6 @@ const {
 } = require("../src/extract_files");
 
 describe("extractlinks", () => {
-  beforeAll(() => {
-    // Create a spy on console (console.log in this case) and provide some mocked implementation
-    // In mocking global objects it's usually better than simple `jest.fn()`
-    // because you can `unmock` it in clean way doing `mockRestore`
-    jest.spyOn(console, "error").mockImplementation(() => {});
-  });
-  afterAll(() => {
-    // Restore mock after all tests are done, so it won't affect other test suites
-    console.error.mockRestore();
-  });
-  afterEach(() => {
-    // Clear mock (all calls etc) after each test.
-    // It's needed when you're using console somewhere in the tests so you have clean mock each time
-    console.error.mockClear();
-  });
   it("resolves with the files' names from a path directory", () => {
     return expect(
       extractFiles("C:\\Users\\Usuario\\md-links\\prueba")
@@ -74,24 +59,4 @@ describe("extractlinks", () => {
       completePathsExtractLinks("C:\\Users\\Usuario\\md-links\\empty_directory")
     ).rejects.toThrow(Error);
   });
-  /*it("rejects with an error for an unvalid file in a directory", () => {
-    return expect(
-      completePathsExtractLinks(
-        "C:\\Users\\Usuario\\md-links\\prueba_test_invalid"
-      )
-    ).resolves.toContain([
-         Error(
-          "\nThe file 'C:\\Users\\Usuario\\md-links\\prueba_test_invalid\\prueba_test_invalid.js' is not markdown! ❎\n"
-        ),
-    ]);
-  });*/
-  /*it("rejects with an error for an unvalid file in a directory", () => {
-    return expect(
-      completePathsExtractLinks(
-        "C:\\Users\\Usuario\\md-links\\prueba_test_invalid"
-      )
-    ).resolves.toMatchObject([
-        Error("\nThe file 'C:\\Users\\Usuario\\md-links\\prueba_test_invalid\\prueba_test_invalid.js' is not markdown! ❎\n"),
-    ]);
-  });  */
 });
--- test/md-links.spec.js
+++
@@ -1,7 +0,0 @@
-const mdLinks = require("../main_function");
-
-describe("mdLinks", () => {
-  it("should...", () => {
-    console.log("FIX ME!");
-  });
-});
